d6e374351e0f45c7ac3d57b6c447536a2cdb24cd
Vlad <cojanvladandrei@yahoo.com>
2023-11-23 16:02:09 +0200

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.components";
+
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const App = () => {
-  return <Directory></Directory>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<div>Shop</div>}></Route>
+        <Route path="sign-in" element={<SignIn />}></Route>
+      </Route>
+    </Routes>
+  );
 };
+
 export default App;
---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import Crwn from "../../assets/007-crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img src={Crwn} alt="" className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/shop" className="nav-link">
+            SHOP
+          </Link>
+          <Link to="/sign-in" className="nav-link">
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet></Outlet>
+    </Fragment>
+  );
+};
+
+export default Navigation;
