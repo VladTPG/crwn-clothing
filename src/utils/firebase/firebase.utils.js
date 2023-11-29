936ab4112b29eec5de6fce137713897afc9450de
Vlad Cojan <cojanvladandrei@yahoo.com>
2023-11-29 15:17:11 +0200

    awdsasd

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADINujV0Am4Jv2NbHaMZeJBZtvEpnJxuY",
+  authDomain: "crwn-clothing-db-95a0d.firebaseapp.com",
+  projectId: "crwn-clothing-db-95a0d",
+  storageBucket: "crwn-clothing-db-95a0d.appspot.com",
+  messagingSenderId: "178062639572",
+  appId: "1:178062639572:web:60259417183633027f9641",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
